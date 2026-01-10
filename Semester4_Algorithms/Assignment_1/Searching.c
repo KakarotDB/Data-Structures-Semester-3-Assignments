85c888888a88449a57352b9098ff4be85d91d1cb
KakarotDB <mrigajpersonal@gmail.com>
2026-01-10 15:59:13 +0530

    Searching.c modified

--- Semester4_Algorithms/Assignment_1/Searching.c
+++ Semester4_Algorithms/Assignment_1/Searching.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 void swap(int *a, int *b) {
     if (a == NULL || b == NULL)
         return;
@@ -47,72 +47,70 @@ void quickSort(int *a, int low, int high) {
 }
 
 int main() {
-	//variable declaration zone
-	int mid = 0;
-	int left = 0;
-	int n = 50000;
-	int right = n - 1;
-	int target = 0;
-	int index = -1;
-	clock_t start, end;
-	int comparisons_linear = 0;
-	int comparisons_binary = 0;
-	int array[n];
-	srand(time(NULL));
-	for(int i = 0; i < n; i++) {
-		array[i] = rand();
-	}
-	printf("Enter target element: ");
-	scanf("%d", &target);
-	printf("Performing linear search!\n");
-	start = clock();
-	for(int i = 0; i < n; i++) {
-		comparisons_linear++;
-		if(array[i] == target){
-			index = i;
-			break;
-		}
-	}
-	end = clock();
-	if(index == -1) {
-		printf("Could not find target %d\n", target);
-	} else {
-		printf("Target element found at position %d\n", index);
-	}
-	printf("Number of comparisons : %d\n", comparisons_linear);
-	printf("Time taken: %f\n", (double) (end - start) / CLOCKS_PER_SEC);
+    // variable declaration zone
+    int mid = 0;
+    int left = 0;
+    int n = 50000;
+    int right = n - 1;
+    int target = 0;
+    int index = -1;
+    clock_t start, end;
+    int comparisons_linear = 0;
+    int comparisons_binary = 0;
+    int array[n];
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        array[i] = rand();
+    }
+    printf("Enter target element: ");
+    scanf("%d", &target);
+    printf("Performing linear search!\n");
+    start = clock();
+    for (int i = 0; i < n; i++) {
+        comparisons_linear++;
+        if (array[i] == target) {
+            index = i;
+            break;
+        }
+    }
+    end = clock();
+    if (index == -1) {
+        printf("Could not find target %d\n", target);
+    } else {
+        printf("Target element found at position %d\n", index);
+    }
+    printf("Number of comparisons : %d\n", comparisons_linear);
+    printf("Time taken: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-	printf("Performing binary search!\n");
-	printf("First sorting the array!\n"); 
-	start = clock();
-	quickSort(array, 0, n); 
-	end = clock();
-	printf("Sorted the array!\n");
-	printf("Time taken to sort array : %f\n", (double) (end - start) / CLOCKS_PER_SEC);
-	start = clock();
-	while(left < right) {
-		mid = (left + right) >> 1;
-		if(target < array[mid]) {
-			comparisons_binary++;
-			right = mid - 1;
-		}
-		else if(target > array[mid]) {
-			comparisons_binary++;
-			left = mid + 1;
-		}
-		else {
-			index = mid;
-			break;
-		}
-	}
-	end = clock();
-	if(index == -1) {
-		printf("Could not find target %d!\n", target);
-	} else {
-		printf("Found target at position %d\n", index);
-	}
-	printf("Number of comparisons : %d\n", comparisons_binary);
-	printf("Time taken: %f\n", (double) (end - start) / CLOCKS_PER_SEC);
-	return 0;
+    printf("Performing binary search!\n");
+    printf("First sorting the array!\n");
+    start = clock();
+    quickSort(array, 0, n);
+    end = clock();
+    printf("Sorted the array!\n");
+    printf("Time taken to sort array : %f\n",
+           (double)(end - start) / CLOCKS_PER_SEC);
+    start = clock();
+    while (left < right) {
+        mid = (left + right) >> 1;
+        if (target < array[mid]) {
+            comparisons_binary++;
+            right = mid - 1;
+        } else if (target > array[mid]) {
+            comparisons_binary++;
+            left = mid + 1;
+        } else {
+            index = mid;
+            break;
+        }
+    }
+    end = clock();
+    if (index == -1) {
+        printf("Could not find target %d!\n", target);
+    } else {
+        printf("Found target at position %d\n", index);
+    }
+    printf("Number of comparisons : %d\n", comparisons_binary);
+    printf("Time taken: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    return 0;
 }
-
