c57cec8de39070c2d5d0f48be6e71cb7c0849099
KakarotDB <mrigajpersonal@gmail.com>
2026-01-21 19:14:59 +0530

    Formatting updated

--- Semester4_Programming_Paradigms/Assignment2/Problem2/GrowingStack.cpp
+++ Semester4_Programming_Paradigms/Assignment2/Problem2/GrowingStack.cpp
@@ -1,41 +1,36 @@
 #include <iostream>
-#define Upper_Bound (int) 1e5
+#define Upper_Bound (int)1e5
 using std::cin, std::cout, std::endl;
 
 namespace StackSystem {
-	struct Stack {
-		int* array;
-		int top;
-		int size;
-		int currentMaxSize;
-		int upperBound;
-	};
+struct Stack {
+    int *array;
+    int top;
+    int size;
+    int currentMaxSize;
+    int upperBound;
+};
 
-	bool initializeStack(Stack &s, const int n) {
-		s.array = NULL;
-		s.array = new int[n];
-		if(s.array == NULL) {
-			return false;
-		}
-		s.top = -1;
-		s.size = 0;
-		s.currentMaxSize = n;
-		s.upperBound = Upper_Bound;
-		return true;
-	}
-
-	bool isEmpty(const Stack &s) {
-		return s.top == -1;
-	}
-	
-	bool isFull(const Stack &s, const int n) {
-		return s.currentMaxSize == n;
-	}
-
-
+bool initializeStack(Stack &s, const int n) {
+    s.array = NULL;
+    s.array = new int[n];
+    if (s.array == NULL) {
+        return false;
+    }
+    s.top = -1;
+    s.size = 0;
+    s.currentMaxSize = n;
+    s.upperBound = Upper_Bound;
+    return true;
 }
 
+bool isEmpty(const Stack &s) { return s.top == -1; }
+
+bool isFull(const Stack &s, const int n) { return s.currentMaxSize == n; }
+
+} // namespace StackSystem
+
 int main() {
-	cout << "Hello world" << endl;
-	return 0;
+    cout << "Hello world" << endl;
+    return 0;
 }
