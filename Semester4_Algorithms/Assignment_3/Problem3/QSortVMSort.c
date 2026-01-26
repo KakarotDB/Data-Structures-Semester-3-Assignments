c7e215ef67815bbc513cb46797bd321de8ed9301
KakarotDB <mrigajpersonal@gmail.com>
2026-01-26 11:51:32 +0530

    Assignment_3\Problem3 started

---
+++ Semester4_Algorithms/Assignment_3/Problem3/QSortVMSort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define ll long long
+
+typedef struct {
+    ll comparisons;
+    int maxDepth;
+    double timeTaken;
+} sortMetrics;
