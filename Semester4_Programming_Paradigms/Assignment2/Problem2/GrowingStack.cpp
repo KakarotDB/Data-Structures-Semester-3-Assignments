406322dba41937dd3cc9cc57d2c785c0d53a492b
KakarotDB <mrigajpersonal@gmail.com>
2026-01-28 19:02:19 +0530

    Finished growing stack

--- Semester4_Programming_Paradigms/Assignment2/Problem2/GrowingStack.cpp
+++ Semester4_Programming_Paradigms/Assignment2/Problem2/GrowingStack.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #define sizeExtend 16
-#define Upper_Bound (int) 50
+#define Upper_Bound (int)1e5
 using std::cin, std::cout, std::endl;
 
 namespace StackSystem {
@@ -25,102 +25,120 @@ bool initializeStack(GrowingStack &s, const int n) {
     return true;
 }
 
+// default parameters to allow default and custom inflation to the stack
+bool inflate(GrowingStack &s, int = sizeExtend);
+
 inline bool isEmpty(const GrowingStack &s) { return s.top == -1; }
 
-inline bool isFull(const GrowingStack &s) { return s.currentSize == s.currentMaxSize; }
+inline bool isFull(const GrowingStack &s) {
+    return s.currentSize == s.currentMaxSize;
+}
 
-inline bool isMaxCapacity(const GrowingStack &s) {	return s.currentSize == s.upperBound; }
+inline bool isMaxCapacity(const GrowingStack &s) {
+    return s.currentSize == s.upperBound;
+}
 
-bool inflate(GrowingStack &s) { //inflates by a fixed amount of 64 bytes (16 integers)
-	if(s.currentMaxSize >= s.upperBound) return false;
-	int newCapacity = s.currentSize + sizeExtend;
+bool inflate(GrowingStack &s, int amount) {
+    // default : Inflates by 64 bytes (16 integers)
+    if (s.currentMaxSize >= s.upperBound)
+        return false;
+    int newCapacity = s.currentSize + amount;
 
-	int *newStackArray = new int[newCapacity];
-	if(!newStackArray) return false;
+    int *newStackArray = new int[newCapacity];
+    if (!newStackArray)
+        return false;
 
-	//this is O(n). There is quite overhead as there is new allocation -> copy
-	for(int i = 0; i < s.currentSize; i++) newStackArray[i] = s.array[i];
-	for(int i = s.currentSize; i < newCapacity; i++) newStackArray[i] = 0;
+    // this is O(n). There is overhead as there is new allocation -> copy
+    for (int i = 0; i < s.currentSize; i++)
+        newStackArray[i] = s.array[i];
+    for (int i = s.currentSize; i < newCapacity; i++)
+        newStackArray[i] = 0;
 
-	delete[] s.array;
-	s.array = newStackArray;
-	s.currentSize = s.currentMaxSize;
-	s.currentMaxSize = newCapacity;
+    delete[] s.array;
+    s.array = newStackArray;
+    s.currentSize = s.currentMaxSize;
+    s.currentMaxSize = newCapacity;
 
-	return true;
+    return true;
 }
 
 bool push(GrowingStack &s, const int value) {
-	if(isMaxCapacity(s)) return false;
-	if(isFull(s)) {
-		if(!inflate(s)) return false;
-	}
-	s.array[++s.top] = value;
-	s.currentSize++;
-	return true;
+    if (isMaxCapacity(s))
+        return false;
+    if (isFull(s)) {
+        if (!inflate(s))
+            return false;
+    }
+    s.array[++s.top] = value;
+    s.currentSize++;
+    return true;
 }
 
 bool pop(GrowingStack &s, int &poppedValue) {
-	if(isEmpty(s)) return false;
+    if (isEmpty(s))
+        return false;
 
-	poppedValue = s.array[s.top];
-	s.array[s.top--] = 0;
-	s.currentSize--;
+    poppedValue = s.array[s.top];
+    s.array[s.top--] = 0;
+    s.currentSize--;
 
-	return true;
+    return true;
 }
 
 } // namespace StackSystem
 
 int main() {
-	using namespace StackSystem;
+    using namespace StackSystem;
 
-	int n = 20;
-	int counter = 1, poppedValue = -1;
-	GrowingStack stack1, stack2, stack3;
+    int n = 20;
+    int counter = 1, poppedValue = -1;
+    GrowingStack stack1, stack2, stack3;
 
-	initializeStack(stack1, n);
-	initializeStack(stack2, n);
-	initializeStack(stack3, n);
+    initializeStack(stack1, n);
+    initializeStack(stack2, n);
+    initializeStack(stack3, n);
 
-	for(int i = 0; i < n; i++) push(stack1, i + 1);
-	for(int i = 0; i < n; i++) push(stack2, i + n + 1);
+    for (int i = 0; i < n; i++)
+        push(stack1, i + 1);
+    for (int i = 0; i < n; i++)
+        push(stack2, i + n + 1);
 
-	cout << "Stack 1 elements : " << endl;
+    cout << "Stack 1 elements : " << endl;
 
-	for(int i = 0; i < n; i++) cout << stack1.array[i] << " ";
-	cout << endl;
+    for (int i = 0; i < n; i++)
+        cout << stack1.array[i] << " ";
+    cout << endl;
 
-	cout << "Stack 2 elements : " << endl;
+    cout << "Stack 2 elements : " << endl;
 
-	for(int i = 0; i < n; i++) cout << stack2.array[i] << " ";
-	cout << endl;
-	
-	while(!isEmpty(stack1) && !isEmpty(stack2)) {
-		if(counter % 2 != 0) {
-			pop(stack1, poppedValue);
-			push(stack3, poppedValue);
-		} else {
-			pop(stack2, poppedValue);
-			push(stack3, poppedValue);
-		}
-		counter++;
-	}
-	while(!isEmpty(stack1)) {
-		pop(stack1, poppedValue);
-		push(stack3, poppedValue);
-	}
-	while(!isEmpty(stack2)) {
-		pop(stack2, poppedValue);
-		push(stack3, poppedValue);
-	}
+    for (int i = 0; i < n; i++)
+        cout << stack2.array[i] << " ";
+    cout << endl;
 
-	cout << "Stack 3 currentMaxSize : " << stack3.currentMaxSize << endl;
-	cout << "Stack 3 currentSize : " << stack3.currentSize << endl;
-	cout << "Stack 3 elements : " << endl;
-	
-	for(int i = 0; i < stack3.currentSize; i++) cout << stack3.array[i] << " ";
-	cout << endl;
+    while (!isEmpty(stack1) && !isEmpty(stack2)) {
+        if (counter % 2 != 0) {
+            pop(stack1, poppedValue);
+            push(stack3, poppedValue);
+        } else {
+            pop(stack2, poppedValue);
+            push(stack3, poppedValue);
+        }
+        counter++;
+    }
+    while (!isEmpty(stack1)) {
+        pop(stack1, poppedValue);
+        push(stack3, poppedValue);
+    }
+    while (!isEmpty(stack2)) {
+        pop(stack2, poppedValue);
+        push(stack3, poppedValue);
+    }
 
-	return 0;
+    cout << "Stack 3 elements : " << endl;
+
+    for (int i = 0; i < stack3.currentSize; i++)
+        cout << stack3.array[i] << " ";
+    cout << endl;
+
+    return 0;
 }
