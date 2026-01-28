afeedb7b4605cdcc6310243eb2e1a722a66b317e
KakarotDB <mrigajpersonal@gmail.com>
2026-01-28 19:19:06 +0530

    Finished genericSwap.c

--- Semester4_Programming_Paradigms/Assignment2/Problem3/SwapGeneric.cpp
+++ Semester4_Programming_Paradigms/Assignment2/Problem3/SwapGeneric.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
-using std::cout, std::cin, std::endl;
+#include <string>
+using std::cout, std::endl, std::string, std::boolalpha;
 
 namespace SwapUtility {
-	template <typename T>
-    void swap(T &a, T &b) {
-		T temp = a;
-		a = b;
-		b = temp;
-	}
-	struct structure {
-		int x; int y;
-	};
+template <typename T> void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
 }
+struct structure {
+    int x;
+    int y;
+};
+} // namespace SwapUtility
 
 int main() {
-	using namespace SwapUtility;
+    using namespace SwapUtility;
 
-	int a = 10, b = 20;
-	float x = 1.1, y = 2.2;
-	structure A, B;
-	A.x = a;
-	A.y = b;
+    int i1 = 10, i2 = 20;
+    float f1 = 1.5f, f2 = 4.5f;
+    double d1 = 99.99, d2 = 0.01;
+    char c1 = 'A', c2 = 'Z';
+    string s1 = "Hello", s2 = "World";
+    bool b1 = true, b2 = false;
+    structure A = {100, 200};
+    structure B = {300, 400};
 
-	B.x = x;
-	B.y = y;
-	
-	cout << "Numbers before swapping : " << endl;
-	cout << "a=" << a << " " << "b=" << b << endl;
-	cout << "x=" << x << " " << "y=" << y << endl;
+    cout << "BEFORE SWAP" << endl;
+    cout << "Integer: " << i1 << ", " << i2 << endl;
+    cout << "Float: " << f1 << ", " << f2 << endl;
+    cout << "Double: " << d1 << ", " << d2 << endl;
+    cout << "Char: " << c1 << ", " << c2 << endl;
+    cout << "String: " << s1 << ", " << s2 << endl;
+    cout << "Bool: " << boolalpha << b1 << ", " << b2 << endl;
+    cout << "Struct: {" << A.x << "," << A.y << "} , {" << B.x << "," << B.y
+         << "}" << endl;
 
-	cout << "Structures before swapping : " << endl;
-	cout << "A.x=" << A.x << " " << "A.y=" << endl;
+    swap(i1, i2);
+    swap(f1, f2);
+    swap(d1, d2);
+    swap(c1, c2);
+    swap(s1, s2);
+    swap(b1, b2);
+    swap(A, B);
 
+    cout << endl << "AFTER SWAP" << endl;
+    cout << "Integer: " << i1 << ", " << i2 << endl;
+    cout << "Float: " << f1 << ", " << f2 << endl;
+    cout << "Double: " << d1 << ", " << d2 << endl;
+    cout << "Char: " << c1 << ", " << c2 << endl;
+    cout << "String: " << s1 << ", " << s2 << endl;
+    cout << "Bool: " << b1 << ", " << b2 << endl;
+    cout << "Struct: {" << A.x << "," << A.y << "} , {" << B.x << "," << B.y
+         << "}" << endl;
 
-	swap(a, b);
-	swap(x, y);
-
-	cout << "Numbers after swapping : " << endl;
-	cout << "a=" << a << " " << "b=" << b << endl;
-	cout << "x=" << x << " " << "y=" << y << endl;
-
-	return 0;
+    return 0;
 }
